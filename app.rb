71c174031aba11bed18ce294ef73e2dcd127274f
HUYQUANGTRAN2 <64404164+HUYQUANGTRAN2@users.noreply.github.com>
2025-04-19 05:13:13 +0000

    1st commit

--- app.rb
+++ app.rb
@@ -5,33 +5,43 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
-end
+end 
+
 
 get("/wizard_add") do
-  @first_num = params.fetch("first_num").to_f
-
-  @result = @first_num + @second_num
+  if params.key?("first_num") && params.key?("second_num")
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num + @second_num
+  end 
   erb(:add_result)
 end
 
 get("/subtract") do
-  erb(:sub_form)
+  erb(:subtraction_form)
 end
 
 get("/wizard_subtract") do
-  @first_num = params.fetch("first_num").to_f
-  @second_num = params.fetch("second_num").to_f
-
+  if params.key?("first_number") && params.key?("second_number")
+    @first_num = params.fetch("first_number").to_f
+    @second_num = params.fetch("second_number").to_f
+    @result = @second_num - @first_num
+  end 
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  if params.key?("first_number") && params.key?("second_number")
+    @first_num = params.fetch("first_number").to_f
+    @second_num = params.fetch("second_number").to_f
+    @result = @second_num * @first_num
+  end 
   erb(:mult_result)
 end
 
@@ -40,5 +50,10 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+  if params.key?("first_number") && params.key?("second_number")
+    @first_num = params.fetch("first_number").to_f
+    @second_num = params.fetch("second_number").to_f
+    @result = @first_num /@second_num
+  end 
   erb(:div_result)
 end
